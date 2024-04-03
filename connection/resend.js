f6f68146aff134f32a79ebe575a61cd0c7f77003
Felipe Escalona <escalonaf12@gmail.com>
2024-04-03 16:03:40 -0400

    resend

--- connection/resend.js
+++ connection/resend.js
@@ -5,7 +5,7 @@ const resend = new Resend("re_Q6W9YHzr_CiEvDvhqznd36GFfaJNsZ8EU");
 export async function correo() {
   const { data, error } = await resend.emails.send({
     from: "Symtechven <soporte@symtechven.com>",
-    to: [`mospina@unimet.edu.ve,escalonaf12@gmail.com`],
+    to: ["mospina@unimet.edu.ve", "escalonaf12@gmail.com"],
     subject: "Confirmacion de asistencia al evento",
     html: `
     <body style="width: 738px; margin: 0 auto">
