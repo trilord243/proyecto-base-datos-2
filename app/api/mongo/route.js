4ab59a3da0b86afc94f9256f0001dfe3a6f34b4f
Felipe Escalona <escalonaf12@gmail.com>
2024-04-03 07:01:18 -0400

    fixings

--- app/api/mongo/route.js
+++ app/api/mongo/route.js
@@ -1,8 +1,12 @@
 import { Pedidos } from "@/connection/pedido";
 import { NextResponse } from "next/server";
 
-export async function GET(req, res) {
+export async function GET(req) {
   const data = await Pedidos.find({});
 
-  return NextResponse.json(data);
+  const response = NextResponse.json(data);
+
+  response.headers.set("Cache-Control", "no-store, max-age=0");
+
+  return response;
 }
